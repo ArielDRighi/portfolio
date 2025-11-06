51d0114a401f462e28a4c3612c85373a3c4ccfbe
Ariel Righi <arieldavidrighi@gmail.com>
2025-11-06 16:03:30 -0300

    fix: remove duplicate content from experience.json and unused imports from main.js

--- src/main.js
+++ src/main.js
@@ -14,9 +14,7 @@ import './sections/projects/projects-states.css';
 import './css/accessibility.css';
 import './css/responsive.css';
 
-// Import JavaScript modules with ES6 exports
-import { initializeThemeManager } from './js/theme-manager.ts';
-import { initializePortfolio } from './js/main.ts';
+// Import JavaScript modules
 import { initializeAccessibility } from './js/accessibility.ts';
 
 // Import other modules (will be converted to ES6 modules in future tasks)
